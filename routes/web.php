54d6c4d5925d6363f57f634fd531340e40327abe
Adiwijaya76 <insaya55@gmail.com>
2025-11-06 22:38:22 +0700

    "feat(posts): implement REST routes + JSON responses"

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\Post\PostController; 
 
 Route::get('/', function () {
     return Inertia::render('welcome');
@@ -11,6 +12,17 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+});
+
+Route::resource('posts', PostController::class)->only([
+    'index', 'create', 'show', 'edit'
+]);
+
+// Write ops (wajib auth)
+Route::middleware('auth')->group(function () {
+    Route::post('posts', [PostController::class, 'store'])->name('posts.store');
+    Route::match(['put', 'patch'], 'posts/{post}', [PostController::class, 'update'])->name('posts.update');
+    Route::delete('posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 });
 
 require __DIR__.'/settings.php';
